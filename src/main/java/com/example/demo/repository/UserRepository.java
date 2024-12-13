65eea69317be867d42bfff4a4d296b957854af2a
Sepehr <sepehristaken@gmail.com>
2024-12-13 01:28:14 -0800

    Add repository

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.example.demo.repository;
+
+import com.example.demo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    User findByUsername(@Param("username") String username);
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    User findByEmail(@Param("email") String email);
+
+    @Query("SELECT u FROM User u WHERE u.username = :username AND u.password = :password")
+    User login(@Param("username") String username, @Param("password") String password);
+
+}
+
